61e80c72d365f1d8ce129ed9bfbbe46e3d851def
liutku5 <l1utkuz20@gmail.com>
2024-06-26 14:37:32 +0300

    Initial commit

---
+++ src/test/java/ElemtTest.java
@@ -0,0 +1,54 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Ignore;
+import org.testng.annotations.Test;
+
+//vardas 3-20 simbolių, raidės skaičiai ir tarpai galimi
+//passwordas 8 simboliai maža, didelė raidė, skaicius, 8 simboliai. viskas kartu privaloma
+import java.time.Duration;
+
+public class ElemtTest {
+    public static WebDriver driver;
+
+    @Test
+    public void websiteRegistration() {
+        driver.get("https://elenta.lt/registracija");
+        WebElement usernameField = driver.findElement(By.name("UserName"));
+        String uesername = "Petras";
+        usernameField.sendKeys(uesername);
+        WebElement emailField = driver.findElement(By.name("Email"));
+        String email = "Petras@gmail.com";
+        emailField.sendKeys(email);
+        WebElement passwordField = driver.findElement(By.name("Password"));
+        String password = "Petras123";
+        passwordField.sendKeys(password);
+        WebElement passwordFieldRepeat = driver.findElement(By.name("Password2"));
+        String passwordRepeat = "Petras123";
+        passwordFieldRepeat.sendKeys(passwordRepeat);
+        driver.findElement(By.xpath("//*[@id=\"main-container\"]/form/fieldset/table/tbody/tr[11]/td[2]/input")).click();
+    }
+
+    public void acceptCookies() {
+        driver.get("https://elenta.lt");
+        driver.findElement(By.className("fc-cta-consent")).click();
+    }
+
+    @BeforeClass
+    public void setUp() {
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+        acceptCookies();
+    }
+
+
+    @AfterClass
+    public void tearDown() {
+//        driver.close();
+    }
+}
